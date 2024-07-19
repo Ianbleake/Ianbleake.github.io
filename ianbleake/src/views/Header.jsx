3f328c7ddacc4852cbc54804a269a278bcb14e26
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-07-19 17:03:56 -0600

    add Header

--- ianbleake/src/views/Header.jsx
+++ ianbleake/src/views/Header.jsx
@@ -1,31 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Logo from '../Assets/BleakeDevLogo.png'
+import Logo from '../Assets/mainowl.svg'
+import { VscGithub } from "react-icons/vsc";
+import { FaLinkedin } from "react-icons/fa";
+import { AiFillTikTok } from "react-icons/ai";
 
 const Header = () => {
   return (
     <header>
 
+        <ul className='rs' >
+            <Link to="" ><li><VscGithub className='icon' /></li></Link>
+            <Link to="" ><li><FaLinkedin className='icon' /></li></Link>
+            <Link to="" ><li><AiFillTikTok className='icon' /></li></Link>
+        </ul>
+
         <Link to="/" >
             <div className='logo'>
-                <img src={Logo} className='headerlogo' />
+            <img src={Logo} className='headerlogo' />
                 <h1 className='logotitle' >BleakeDev</h1>
             </div>
         </Link>
 
-        <nav>
-            <ul id="navigation" className='mainmenu'>
-                <li className='mainlink' >
-                    <Link to="/">Home</Link>
-                </li>
-                <li className='mainlink'>
-                    <Link to="/about">About</Link>
-                </li>
-                <li className='mainlink'>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
+        <ul id="navigation" className='mainmenu'>
+            <li className='mainlink'>
+                <Link to="/about">About</Link>
+            </li>
+            <li className='mainlink'>
+                <Link to="/contact">Contact</Link>
+            </li>
+            <li className='mainlink'>
+                <Link to="/contact">Blog</Link>
+            </li>
+        </ul>
+
     </header>
   )
 }
